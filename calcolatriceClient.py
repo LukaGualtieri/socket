ec07f92726d91dbbce573dd69da65c509b919570
LukaGualtieri <luca.gualtieri@alessandrinimainardi.edu.it>
2024-01-12 09:44:10 +0100

    TCP

--- calcolatriceClient.py
+++ calcolatriceClient.py
@@ -1,1 +1,31 @@
+import socket,json
 
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 5005
+BUFFER_SIZE = 1024
+NUM_MESSAGES = 5
+
+#Creazione del socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+for i in range(NUM_MESSAGES):
+    #Invio delmessaggio al server
+    message = "ping"
+    sock.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
+    print(f"Messaggio inviato al server: {message}")
+
+    #Ricezione della risposta dal server 
+    data, addr = sock.recvfrom(BUFFER_SIZE)
+    print(f"Messaggio ricevuto dal server {addr}: {data.decode()}")
+
+#Chiusura del socket
+sock.close()
+
+data=cs.recv(1024)
+#if len(data) ==0:   
+    break
+data=data.decode()
+data=json.loads(data)
+primoNumero=data['primoNumero']
+operazione=data['operazione']
+secondoNumero=data['secondoNumero']
--- calcolatriceServer.py
+++ calcolatriceServer.py
@@ -1,1 +1,33 @@
+import socket,json
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 5005
+BUFFER_SIZE = 1024
+
+#Creazione del socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((SERVER_IP, SERVER_PORT))
+
+primoNumero=float(primoNumero)
+operazione=input("Inserisci l'operazione: (+,-,*,/,%)")
+secondoNumero=float(input("Inserisci il secondo numero: "))
+messaggio={'primoNumero': primoNumero,
+           'operazione': operazione,
+           'secondoNumero': secondoNumero}
+messaggio=json.dumps(messaggio) #trasformiamo l'oggetto in stringa
+s.sendall(messaggio.encode("UTF-8"))
+data=s.recv(1024)
+print("Risultato: ",data.decode())
+
+print("Server in attesa di messaggi...")
+
+while True:
+    #Ricezione dei dati dal client
+    data, addr = sock.recvfrom(BUFFER_SIZE)
+    print(f"Messaggio ricevuto dal client {addr}: {data.decode()}")
+
+    #Invio di una risposta al client
+    reply = "pong"
+    sock.sendto(reply.encode(), addr)
+
 
